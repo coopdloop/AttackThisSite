3e608d7e2ae27fe0552fada78b638d5e4fcd880c
unknown <cooper.wallace32599@gmail.com>
2020-02-28 21:26:05 -0600

    GETREQ

--- start.py
+++ start.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home', methods = ['GET','POST'])
 def home():
+    if request.args.get('fname') and request.args.get('lname'):
+        f = request.args.get('fname')
+        l = request.args.get('lname')
+        print(f + " " + l)
     return render_template('home.html')
+    
+def sqlsend():
+    
+    return 
 
 
 if __name__ == '__main__':
