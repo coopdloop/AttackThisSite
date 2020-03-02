c2d7cd1bc46372ff2293a6c1cc158e0ccf093679
coopdloop <cooper.wallace32599@gmail.com>
2020-03-01 20:59:17 -0500

    finished

--- sql/sql-dump/init_db.sql
+++ sql/sql-dump/init_db.sql
@@ -4,3 +4,15 @@ CREATE TABLE `names` (
   `last` varchar(255) default NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1;
+
+
+
+CREATE TABLE `secret` (
+  `secret id` mediumint(8) unsigned NOT NULL auto_increment,
+  `secret first` varchar(255) default NULL,
+  `secret last` varchar(255) default NULL,
+  PRIMARY KEY (`secret id`)
+) AUTO_INCREMENT=1;
+
+
+INSERT into `secret` (`secret first`,`secret last`) VALUES ("cooper","j");
--- start.py
+++ start.py
@@ -7,16 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home', methods = ['GET','POST'])
 def home():
+
     if request.args.get('fname') and request.args.get('lname'):
         f = request.args.get('fname')
         l = request.args.get('lname')
         sqlsend(f,l)
         #print(f + " " + l)
-    return render_template('home.html')
+    elif request.args.get('searchreq'):
+        s = request.args.get('searchreq')
+        
+        out = sqlsearch(s)
+        return render_template('home.html',out=out)
+    return render_template('home.html', out='Your results will show here...')
     
 def sqlsend(first,last):
     mydb = mysql.connector.connect(
-        host="192.168.56.56",
+        host="127.0.0.1",
         user="coop",
         passwd="secret",
         database="db"
@@ -25,22 +31,51 @@ def sqlsend(first,last):
     mycursor = mydb.cursor()
 
     # Safe from SQli
-    #query = "INSERT INTO names(first, last) VALUES (%s, %s)"
-    #mycursor.execute(query, (first, last))
+    query = "INSERT INTO names(first, last) VALUES (%s, %s)"
+    mycursor.execute(query, (first, last))
 
 
     # Example of unsafe query
-    unsafequery = "select * from names where id = USERINPUT"
-    unsafequery = unsafequery.replace("USERINPUT", "1 or 1=1")
+    #unsafequery = "select * from names where id = USERINPUT"
+    #unsafequery = unsafequery.replace("USERINPUT", "1 or 1=1")
 
-    print("Query : {}".format(unsafequery))
-    mycursor.execute(unsafequery)
-    out = mycursor.fetchall()
+    #print("Query : {}".format(unsafequery))
+    #mycursor.execute(unsafequery)
+    #out = mycursor.fetchall()
 
-    print(out)
+    #print(out)
 
     mydb.commit()
     mycursor.close()
 
+
+def sqlsearch(search):
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="coop",
+        passwd="secret",
+        database="db"
+        #buffered=True
+    )
+
+    mycursor = mydb.cursor()
+    try:
+        unsafequery = "select * from names where id = USERINPUT"
+        unsafequery = unsafequery.replace("USERINPUT", search)
+        print("Your Query:   " +unsafequery)
+        mycursor.execute(unsafequery)
+        out = mycursor.fetchall()
+        print(out)
+
+        mydb.commit()
+        mycursor.close()
+
+        return out
+
+    except:
+        print(unsafequery)
+        return "error"
+
+        
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0' ,port=1337)
